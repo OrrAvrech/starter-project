3bcef745b903d8ebb19f8209276a102503e9b267
Orr Avrech <orravrech@gmail.com>
2023-09-27 09:02:30 -0400

    add train_video_classifier notebook and fix sorted bug in action_cls_dataset.py

--- action_cls/data/action_cls_dataset.py
+++ action_cls/data/action_cls_dataset.py
@@ -16,8 +16,10 @@ class ActionClassificationDataset(Dataset):
         transform=None,
     ):
         self.data_dir = data_dir
-        self.labels = [sub.name for sub in data_dir.iterdir() if sub.is_dir()]
+        self.labels = sorted([sub.name for sub in data_dir.iterdir() if sub.is_dir()])
+        self.num_classes = len(self.labels)
         self.class_to_idx = {cls: idx for idx, cls in enumerate(self.labels)}
+        self.idx_to_class = {idx: cls for idx, cls in enumerate(self.labels)}
         self.clips = []
         self.num_frames = num_frames
         self.sample_rate = sample_rate
@@ -34,23 +36,25 @@ class ActionClassificationDataset(Dataset):
 
     def __getitem__(self, idx) -> tuple[torch.float32, int]:
         video_path, label = self.clips[idx]
-        vid = VideoFileClip(str(video_path))
-        clip_duration = self.num_frames * self.sample_rate / vid.fps
-        subsample_fps = vid.fps / self.sample_rate
 
-        # by default take the middle
-        start_time = vid.duration // 2
-        # for training, random sample
-        if self.random_sampler:
-            start_time = random.uniform(0, vid.duration - clip_duration)
-        end_time = start_time + clip_duration
+        with VideoFileClip(str(video_path)) as vid:
+            clip_duration = self.num_frames * self.sample_rate / vid.fps
+            subsample_fps = vid.fps / self.sample_rate
 
-        video_frames = list(vid.subclip(start_time, end_time).iter_frames(fps=subsample_fps))[
-            : self.num_frames
-        ]
+            # by default take the start
+            # start_time = vid.duration // 2
+            start_time = 0
+            # for training, random sample
+            if self.random_sampler:
+                start_time = random.uniform(0, vid.duration - clip_duration)
+            end_time = start_time + clip_duration
+
+            video_frames = list(vid.subclip(start_time, end_time).iter_frames(fps=subsample_fps))[
+                : self.num_frames
+            ]
+
         video_np = np.moveaxis(np.array(video_frames), [0, -1], [1, 0])
         video_data = {"video": torch.from_numpy(video_np), "audio": None}
         video_data = self.transform(video_data)
         inputs = video_data["video"]
-
         return inputs, label
